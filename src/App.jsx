26dade0dfe681393af62da3be032e43f958f70cd
HabiburRahmanZihad <e241024@ugrad.iiuc.ac.bd>
2025-03-28 12:06:26 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { Suspense } from "react";
+import "./App.css";
+import Countries from "./component/countries/countries";
+
+const countriesPromise = fetch("https://restcountries.com/v3.1/all")
+  .then((response) => response.json())
+
+
+function App() {
+  return (
+    <>
+      <Suspense fallback={<h1>Loading...</h1>}>
+        <Countries countriesPromise={countriesPromise} />
+      </Suspense>
+    </>
+  );
+}
+
+export default App;
